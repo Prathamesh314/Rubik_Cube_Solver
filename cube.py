2f9d74d391c8eef878da5ffce1cbb1c63b11fc26
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2024-02-09 01:43:27 +0530

    completerd left rotate of top face

--- cube.py
+++ cube.py
@@ -101,10 +101,37 @@ class Cube:
                 print(cubes)
             print()
 
-    def rotate_top(dir, row):
+    def rotate_top(self, dir, row):
         if dir == -1:
             # move Left
-            # Let us first find 
-
+            # Let us first find
+            # If it is top row then bottom face wont be affected
+            if row == 0:
+                face = self.scrambled_cube[self.dirs["Face"]]
+                right =  self.scrambled_cube[self.dirs["Right"]]
+                left =  self.scrambled_cube[self.dirs["Left"]]
+                back =  self.scrambled_cube[self.dirs["Back"]]
+                top =  self.scrambled_cube[self.dirs["Top"]]
+                temp = face[0]
+                face[0] = right[0]
+                right[0] = back[0]
+                back[0] = left[0]
+                left[0] = temp
+                
+                # rotate top face to Left
+                
+                new_top = [[0]*3 for _ in range(3)]
+                n = len(face)
+                m = len(face[0])
+                for i in range(n):
+                    for j in range(m):
+                        new_top[i][j] = top[j][n-i-1]
+                self.scrambled_cube[self.dirs["Top"]] = new_top 
+               
 cube = Cube(2)
+print("Original Scrambled Cube")
 cube.show_scramble_cube()
+cube.rotate_top(-1, 0)
+print("After rotating top Face")
+cube.show_scramble_cube()
+
