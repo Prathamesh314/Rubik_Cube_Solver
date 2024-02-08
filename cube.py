7a138fdf8bd73eef465d20596db2e3f556472eb9
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2024-02-09 01:13:34 +0530

    created cube

---
+++ cube.py
@@ -0,0 +1,110 @@
+#print("Hello World")
+
+class Cube:
+    def __init__(self, number):
+        self.cube = []
+        self.colors = {
+            1: "White",
+            2: "Blue",
+            3: "Yellow",
+            4: "Green",
+            5: "Orange",
+            6: "Red"
+        }
+        self.dirs = {
+            "Face": 0,
+            "Top": 1,
+            "Back": 2,
+            "Bottom": 3,
+            "Right": 4,
+            "Left": 5
+        }
+        self.scrambled_cube = []
+        self.build_cube(number)
+        self.build_scramble_cube()
+    
+    def build_cube(self, number):
+        # order will be Face, Top, Back, Bottom, Right, Left
+        # This cube is of type 1 where blue is at top and white is at front and orange is at right
+        self.cube = [
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]
+            ],
+            [[2, 2, 2],
+             [2, 2, 2],
+             [2, 2, 2]
+            ],
+            [[3, 3, 3],
+             [3, 3, 3],
+             [3, 3, 3]
+            ],
+            [[4, 4, 4],
+             [4, 4, 4],
+             [4, 4, 4]
+            ],
+            [[5, 5, 5],
+             [5, 5, 5],
+             [5, 5, 5]
+            ],
+             [[6, 6, 6],
+             [6, 6, 6],
+             [6, 6, 6]
+            ],
+        ]
+    
+    def build_scramble_cube(self):
+       self.scrambled_cube = [
+            [
+             [2, 2, 5],
+             [5, 1, 4],
+             [6, 4, 2]
+            ],
+            [
+             [1, 3, 3],
+             [1, 2, 6],
+             [1, 5, 6]
+            ],
+            [
+             [6, 3, 3],
+             [3, 3, 2],
+             [3, 5, 4]
+            ],
+            [
+             [5, 2, 3],
+             [6, 4, 2],
+             [5, 6, 1]
+            ],
+            [
+             [2, 4, 5],
+             [3, 5, 1],
+             [6, 1, 4]
+            ],
+            [
+             [5, 2, 3],
+             [6, 4, 2],
+             [5, 6, 1]
+            ],
+           
+        ] 
+
+    def show_cube(self):
+        for faces in self.cube:
+            print(self.colors[faces[0][0]])
+            for cubes in faces:
+                print(cubes)
+            print()
+    
+    def show_scramble_cube(self):
+        for faces in self.scrambled_cube:
+            for cubes in faces:
+                print(cubes)
+            print()
+
+    def rotate_top(dir, row):
+        if dir == -1:
+            # move Left
+            # Let us first find 
+
+cube = Cube(2)
+cube.show_scramble_cube()
