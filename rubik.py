f146be6b67121eb331ec787129b8fc9150280d5e
Prathamesh314 <kurvep18@gmail.com>
2024-04-17 20:59:13 +0530

    Now parent can rotate with keys

--- rubik.py
+++ rubik.py
@@ -14,13 +14,34 @@ camera.world_position = (0, 0, -15)
 
 cube_model, cube_texture = 'models/custom_cube.obj', 'textures/rubik_texture.png'
 
-PARENT = Entity(model=cube_model, texture='textures/chess_texture.png')
+PARENT = Entity(model=cube_model, texture=cube_texture)
 # print(f"{PARENT.position=}")
 rotation_y  = 90
 cube1 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 1, 1))
 
-def update():
-    PARENT.rotation_z += 30*time.dt
+
+
+def input(key):
+    global PARENT
+    if key == 'd':
+        print("I am input key D")
+        PARENT.rotation_y += 90
+    elif key == 'l':
+        print("I am input key L")
+        PARENT.rotation_x += 90
+    elif key == 'r':
+        print("I am input key R")
+        PARENT.rotation_x += 90
+    elif key == 'u':
+        print("I am input key U")
+        PARENT.rotation_y += 90
+    elif key == 'f':
+        print("I am input key F")
+        PARENT.rotation_z += 90
+    elif key == 'b':
+        print("I am input key B")
+        PARENT.rotation_z += 90
+
 
 # runnning the app
 app.run()
