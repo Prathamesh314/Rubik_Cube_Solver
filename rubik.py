2f1ba2b9216748922592050732a7c37e1be67c87
Prathamesh314 <kurvep18@gmail.com>
2024-04-18 18:55:24 +0530

    Created a 3d cube

--- rubik.py
+++ rubik.py
@@ -1,5 +1,4 @@
 from ursina import *
-
 # creating a window
 app = Ursina(fullscreen=True)
 
@@ -18,7 +17,43 @@ PARENT = Entity(model=cube_model, texture=cube_texture)
 # print(f"{PARENT.position=}")
 rotation_y  = 90
 cube1 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 1, 1))
+cube2 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 0, 1))
+cube3 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, -1, 1))
+cube4 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, 1, 1))
+cube6 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, 0, 1))
+cube8 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, -1, 1))
+cube5 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, 1, 1))
+cube7 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, 0, 1))
+cube9 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, -1, 1))
+cube10 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 1, 0))
+cube11 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 0, 0))
+cube12 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, -1, 0))
+cube13 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, 1, 0))
+cube14 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, -1, 0))
+cube15 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, 1, 0))
+cube16 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, 0, 0))
+cube17 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, -1, 0))
+cube18 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 1, -1))
+cube19 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, 0, -1))
+cube20 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(1, -1, -1))
+cube21 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, 1, -1))
+cube22 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, -1, -1))
+cube23 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, 1, -1))
+cube24 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, 0, -1))
+cube25 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(-1, -1, -1))
+cube26 = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(0, 0, -1))
 
+cube_entities = {}
+
+for i in range(-1, 2):
+    for j in range(-1, 2):
+        for k in range(-1, 2):
+            if i == j and j == k and  i == 0:
+                continue
+            cube_entities[(i, j , k)] = Entity(parent=PARENT, model=cube_model, texture=cube_texture, position=(i, j , k))
+
+for i, j in cube_entities.items():
+    print(f"{i} = {j}")
 
 
 def input(key):
